8f4d5fa46f16aa3b36fb4e486799278dd5ddd6dc
Iurii Romanov <yromanov0@gmail.com>
2024-09-02 10:15:24 +0200

    Express

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require("graphql")
+var express = require("express")
+var { createHandler } = require("graphql-http/lib/use/express")
+var { buildSchema } = require("graphql")
  
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -7,18 +9,24 @@ var schema = buildSchema(`
   }
 `)
  
-// The rootValue provides a resolver function for each API endpoint
-var rootValue = {
+// The root provides a resolver function for each API endpoint
+var root = {
   hello() {
     return "Hello world!"
-  }
+  },
 }
  
-// Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: "{ hello }",
-  rootValue
-}).then(response => {
-  console.log(response)
-})
+var app = express()
+ 
+// Create and use the GraphQL handler.
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+)
+ 
+// Start the server at port
+app.listen(4000)
+console.log("Running a GraphQL API server at http://localhost:4000/graphql")
